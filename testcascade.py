631f8267c28f35453384f1abf33b1653db36a691
root <root@LAPTOP-FN25MAIF.localdomain>
2019-06-04 13:39:20 +0000

    Accuracy, recall e precison

--- testcascade.py
+++ testcascade.py
@@ -3,6 +3,7 @@ import cv2
 import time
 import math
 import sys 
+from tabulate import tabulate
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
@@ -25,8 +26,8 @@ file = open(sys.argv[1], "r")
 file_names = file.read()
 #while 1:
 cont = 1
-cont2 = 0
-plate_count = 0
+false_negative = 0
+true_positive = 0
 for name in file_names.split("\n"):
     
     time.sleep(1/30.0)
@@ -44,12 +45,12 @@ for name in file_names.split("\n"):
         #img = cv2.resize(img, (1280,720))
         img = cv2.resize(img, (640, 480))
     except Exception as e:
-        cont2 = cont2 + 1
+        false_negative = false_negative + 1
         continue
     try:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     except Exception as e:
-        #cont2 = cont2 + 1
+        #false_negative = false_negative + 1
         continue
 
     gray = cv2.equalizeHist(gray)
@@ -89,7 +90,7 @@ for name in file_names.split("\n"):
     nx, ny, nw, nh = 0,0,0,0
     
     if len(plates)==0:
-        cont2 = cont2 + 1
+        false_negative = false_negative + 1
         continue
 
     new_name = name.split(".jpg")
@@ -126,10 +127,10 @@ for name in file_names.split("\n"):
         g_truth_triangle_center_x = (x+w + x)/2
         g_truth_triangle_center_y = (y+h + y)/2
 
-        found_triangle_center_x = (plate_positions[2] + plate_positions[0])/2
-        found_triangle_center_y = (plate_positions[3] + plate_positions[1])/2
+        false_positive_triangle_center_x = (plate_positions[2] + plate_positions[0])/2
+        false_positive_triangle_center_y = (plate_positions[3] + plate_positions[1])/2
 
-        euclidean_dist = math.sqrt(math.pow(found_triangle_center_x-g_truth_triangle_center_x, 2) + math.pow(found_triangle_center_y-g_truth_triangle_center_y, 2))
+        euclidean_dist = math.sqrt(math.pow(false_positive_triangle_center_x-g_truth_triangle_center_x, 2) + math.pow(false_positive_triangle_center_y-g_truth_triangle_center_y, 2))
         print("Euclidean dist: " + str(euclidean_dist))
 
         #Verifica se o ground truth está inscrito em uma das regiões de interesse
@@ -138,10 +139,10 @@ for name in file_names.split("\n"):
         if (plate_positions[0] > x and plate_positions[2] < (x + w) and plate_positions[1] > y and plate_positions[3] < (y + h)) or \
         (x > plate_positions[0] and (x + w) < plate_positions[2] and y > plate_positions[1] and (y + h) < plate_positions[3]) or \
         euclidean_dist < 15:
-            print("plate_count: " + str(plate_count))
-            plate_count = plate_count + 1
+            print("true_positive: " + str(true_positive))
+            true_positive = true_positive + 1
             cv2.rectangle(img,(plate_positions[0],plate_positions[1]),(plate_positions[2],plate_positions[3]),(0,255,0),2)
-            cv2.imwrite("./plate_count_images/plate-" + str(cont) + ".jpg", img)
+            cv2.imwrite("./true_positive_images/plate-" + str(cont) + ".jpg", img)
             break
 
     #for (x,y,w,h) in faces:
@@ -163,7 +164,7 @@ for name in file_names.split("\n"):
     except Exception as e:
         new_image = new_image[ny:ny+nh,nx:nx+nw]
 
-    #cv2.imwrite("found_by_cascade3/plate-" + str(cont) + ".jpg", img)
+    #cv2.imwrite("false_positive_by_cascade3/plate-" + str(cont) + ".jpg", img)
     print(name + " " + str(cont))
     
     #cv2.imshow('img',img)
@@ -173,20 +174,21 @@ for name in file_names.split("\n"):
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
-print("Placas perdidas (False Negative): " + str(cont2))
+print("Placas perdidas (False Negative): " + str(false_negative))
 print("Placas com regiões de interesse: " + str(cont))
-print("Imagens em que a placa foi encontrada (True Positive): " + str(plate_count))
+print("Imagens em que a placa foi encontrada (True Positive): " + str(true_positive))
 
-print("Porcentagem de acerto: " + str(int(plate_count*100.0/cont)) + "%")
+print("Porcentagem de acerto: " + str(int(true_positive*100.0/cont)) + "%")
 
 #def evaluate_classifier():
 
+### ROTINA PARA TESTAR A PERFORMANCE NOS DADOS NEGATIVOS (ONDE NÁ HÁ PLACA) ###
 file = open("negatives.txt", "r")
 #file = open("car_info.txt", "r")
 file_names = file.read()
 
-not_found = 0
-found = 0
+true_negative = 0
+false_positive = 0
 for name in file_names.split("\n"):
     
     time.sleep(1/30.0)
@@ -204,12 +206,12 @@ for name in file_names.split("\n"):
         #img = cv2.resize(img, (1280,720))
         img = cv2.resize(img, (640, 480))
     except Exception as e:
-        cont2 = cont2 + 1
+        false_negative = false_negative + 1
         continue
     try:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     except Exception as e:
-        #cont2 = cont2 + 1
+        #false_negative = false_negative + 1
         continue
 
     gray = cv2.equalizeHist(gray)
@@ -220,19 +222,41 @@ for name in file_names.split("\n"):
     nx, ny, nw, nh = 0,0,0,0
     
     if len(plates) is not 0:
-        not_found = not_found + 1
+        true_negative = true_negative + 1
     else:
-        found = found + 1
+        false_positive = false_positive + 1
 
-    print(name + " " + str(cont))
+    print(name + " " + str(false_positive+true_negative))
     
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
-print("Imagens sem placa em que placa não foi identificada (True Negative): " + str(not_found))
-print("Imagens sem placa em que alguma placa foi identificada (False Negative): " + str(found))
+print("True Positive: " + str(true_positive))
+print("False negative: " + str(false_negative))
+print("Imagens sem placa em que placa não foi identificada (True Negative): " + str(true_negative))
+print("Imagens sem placa em que alguma placa foi identificada (False Positive): " + str(false_positive))
+
+results = []
+results.append(("[Valor Real] Placas", true_positive, false_positive))
+results.append(("[Valor Real] Não Placas",   false_negative, true_negative))
+print(tabulate(results, headers=[" ", "[Valor Predito] Placas", "[Valor Predito] Não Placas"]))
+
+accuracy = (true_positive + true_negative)/(true_positive+true_negative+false_negative+false_positive)
+recall = (true_positive/(true_positive+false_negative))
+precision= (true_positive/(true_positive+false_positive)) #
+f_score = (2*precision*recall)/(precision+recall)
 
 
+metrics = []
+metrics.append(("Accuracy", format(accuracy, '.2f')))
+metrics.append(("Recall", format(recall, '.2f')))
+metrics.append(("Precision", format(precision, '.2f')))
+metrics.append(("f-score", format(f_score, '.2f')))
+
+
+print(tabulate(metrics))
+
+print
 
 #cap.release()
 cv2.destroyAllWindows()
